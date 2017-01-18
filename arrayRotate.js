096b05ba9fd14ab1c578ae5a3a8c8f55a265b078
codyw9524 <codyw9524@gmail.com>
2017-01-18 14:38:07 -0600

    first attempt at implementing a trie

--- arrayRotate.js
+++ arrayRotate.js
@@ -1,9 +1,8 @@
 function arrayRotate(arr, n){
 	var temp;
 	if (!arr.length || !n) { return; }
-
   n += Math.ceil(Math.abs(n) / arr.length) * arr.length;
-  console.log(n)
+  console.log('N: ', n)
   while(n--){
   	temp = arr[arr.length - 1];
   	for(var i = arr.length - 2;i > -1; i--){
@@ -11,7 +10,7 @@ function arrayRotate(arr, n){
     }
     arr[0] = temp;
   }
-  return arr
+  return arr;
 }
 
 console.log(arrayRotate([1,2,3,4,5], -9))
---
+++ trie.js
@@ -0,0 +1,45 @@
+function Trie(){
+	this.root = new TrieNode();
+}
+
+function TrieNode(){
+	this.pointers = [];
+	this.value = null;
+}
+
+function objIndexOf(node, char){
+	console.log(node)
+	console.log(node.pointers)
+	for(let i = 0; node.pointers.length; i++){
+		let pointer_node = node.pointers[i];
+		if(pointer_node.hasOwnProperty(char)){
+			return i;
+		}
+	}
+	return -1;
+}
+
+Trie.prototype.insert = function(str){
+	let node = this.root;
+	for(let i = 0; i < str.length; i++){
+		let pointer = objIndexOf(node, str[i])
+		console.log('pointer: ', pointer)
+		while(pointer != -1){
+			node = node.pointers[pointer]
+		}
+		let obj = {}
+		obj[str[i]] = new TrieNode();
+		node.pointers.push(obj)
+		node = node.pointers[node.pointers.length -1]
+		console.log('node: ', node)
+		if(i == str.length - 1 && ! node.value){
+			node.value = str;
+		}
+	}
+	return this;
+}
+
+let trie = new Trie();
+trie.insert('cody');
+console.log(trie)
+
