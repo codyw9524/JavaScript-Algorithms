39a20bb0366d7f8d0326b4d889524c1cf0b08d44
Cody Williams <codyw9524@gmail.com>
2017-02-04 11:52:52 -0600

    added sumOfSquares.js and numToBinary.js

---
+++ numToBinary.js
@@ -0,0 +1,5 @@
+function numToBinary(n){
+    return (n >>> 0).toString(2);
+}
+
+console.log(numToBinary(16));
---
+++ sumOfSquares.js
@@ -0,0 +1,49 @@
+console.time('squares')
+function isSquare(n){
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+}
+
+function rGetSquares(n, arr=[]){
+    if(isSquare(n)){
+        arr.push(n);
+    }
+    if(n == 1){
+        return arr;
+    }
+    return rGetSquares(n - 1, arr);
+}
+
+function permuteArr(arr, n, sub=[], permArr=[]){
+    var length = arr.length;
+    if(sub.length > 0 && sub.reduce((a, b) => a + b) == n){
+        var safePush = true;
+        for(var k = 0; k < permArr.length; k++){
+            var check1 = permArr[k].slice(0);
+            var check2 = sub.slice(0);
+            if(check1.sort().join(',') === check2.sort().join(',')){
+                safePush = false;
+            }
+        }
+        if(safePush === true){
+            permArr.push(sub);
+        }
+    }
+    for(var i = 0; i < length; i++){
+        sub2 = sub.slice(0);
+        sub2.push(arr[i]);
+        var newArr = arr.slice(0);
+        var first = arr.slice(0, i);
+        newArr = first.concat(newArr.splice(i + 1));
+        permuteArr(newArr, n, sub2, permArr);
+    }
+    return permArr;
+}
+
+function sumOfSquares(num){
+    return permuteArr(rGetSquares(num), num);
+    // return rGetSquares(num)
+}
+
+
+console.log(sumOfSquares(100));
+console.timeEnd('squares')
